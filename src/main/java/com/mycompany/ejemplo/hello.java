48553324dccf75aec6f0240ac6aa3ee31fe225c8
MRobot404 <76190109+MRobot404@users.noreply.github.com>
2021-02-25 09:07:19 -0600

    Update hello.java

--- src/main/java/com/mycompany/ejemplo/hello.java
+++ src/main/java/com/mycompany/ejemplo/hello.java
@@ -2,6 +2,7 @@ package com.mycompany.ejemplo;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Friend");
+       System.out.println("that lame"); 
     }
     
 }
